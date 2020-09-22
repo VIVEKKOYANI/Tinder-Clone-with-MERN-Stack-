161de6a152a1118c27760f5ff66f42032330726e
vivek <vivek.koyani@gmail.com>
2020-09-22 13:27:36 +0530

    add get and post req in BackEnd

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,12 +1,16 @@
 import express from "express";
 import mongoose from "mongoose";
 import Cards from "./dbcards.js";
+import bodyParser from 'body-parser';
+import Cors from 'cors';
 //App config
 const app = express();
 const port = process.env.PORT || 8001;
 
 //Middlewares
-app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(Cors());
 //Db config
 const connection_url ='mongodb://admin:zwVrMf0hfVurCfdr@cluster0-shard-00-00.9ort9.mongodb.net:27017,cluster0-shard-00-01.9ort9.mongodb.net:27017,cluster0-shard-00-02.9ort9.mongodb.net:27017/tinderDB?ssl=true&replicaSet=atlas-bwf8t2-shard-0&authSource=admin&retryWrites=true&w=majority';
 
@@ -16,7 +20,7 @@ mongoose.connect(connection_url, {
   useUnifiedTopology: true,
 });
 //Api Endpoints
-app.get("/", (req, res) => res.status(200).send("Hello"));
+app.get("/", (req, res) => res.status(200).json({message: "Hello"}));
 
 app.post("/tinder/cards", (req, res) => {
   const dbCard = req.body;
@@ -25,7 +29,7 @@ app.post("/tinder/cards", (req, res) => {
     if (err) {
       res.status(500).send(err);
     } else {
-      res.status(201).send(data);
+      res.status(201).json(data);
     }
   });
 });
@@ -37,7 +41,7 @@ app.get("/tinder/cards", (req, res) => {
     if (err) {
       res.status(500).send(err);
     } else {
-      res.status(200).send(data);
+      res.status(200).json(data);
     }
   });
 });
